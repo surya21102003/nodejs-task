0f72cbc85c48c5ff1c22e85355b3724f65ce99fa
surya21102003 <156424142+surya21102003@users.noreply.github.com>
2025-07-04 19:04:59 +0530

    Update api.js

--- products-client/src/api.js
+++ products-client/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:"https://nodejs-task-backend.onrender.com",
+  baseURL:"https://nodejs-task-backend.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
